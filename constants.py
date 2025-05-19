ade783d1c94887b8240d16fe80d807e9c6c2f85a
Anwarat Gurung <agurung_01@outlook.com>
2025-05-19 14:35:46 +0100

    Add gravitational constant (G), as well as Earth and Moon mass (in kg).

---
+++ constants.py
@@ -0,0 +1,3 @@
+G           = 6.67430e-11           # gravitational constant (m^3 kg^-1 s^-2)
+M_EARTH     = 5.9722e24             # mass of Earth (kg) 
+M_MOON      = 7.346e22              # mass of Moon (kg) 
