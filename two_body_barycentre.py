e02597673535e85a32107d55f2d47e5dc0f81337
Anwarat Gurung <agurung_01@outlook.com>
2025-06-05 16:33:52 +0100

    Refactor earth_moon_system function subscripts from 'e' & 'm' to generic 1 & 2 to change values for any two-mass system.

--- two_body_barycentre.py
+++ two_body_barycentre.py
@@ -2,65 +2,61 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
-from matplotlib.animation import FuncAnimation, FFMpegWriter
 from matplotlib.ticker import MaxNLocator
 plt.rcParams["font.size"] = 8
 plt.rcParams["font.family"] = "monospace"
 plt.rcParams["lines.linewidth"] = 1
-import time
-import datetime
 from constants import *
-from tqdm_pbar import tqdmFA
 
 
-def earth_moon_system(
+def two_body_system(
+        m1: float = M_EARTH,
+        m2: float = M_MOON,
+        d: float = D_EARTH_MOON,
+        v0: float = V_MOON,
+        i_deg: float = i_MOON,
         T_days: float = 27.321,
         steps: int = 1000,
-        method: str = "RK4"
+        ode_method: str = "RK45"
     ) -> tuple:
-    """
-    Simulate the Earth-Moon system using a two-body problem approach.
-    The simulation uses the barycentric coordinates of the Earth and Moon.
-    """
-    
-    i_rad = np.radians(i_MOON)
 
-    # initial position vectors of Earth and Moon:
-    r_m0 = np.array([D_EARTH_MOON * np.cos(i_rad), 0.0, D_EARTH_MOON * np.sin(i_rad)])      
-    r_e0 = -M_MOON/M_EARTH * r_m0                                                          
+    # initial position vectors of mass 1 and mass 2:
+    i_rad = np.radians(i_deg)   # convert inclination angle to radians
+    r2_0 = np.array([d * np.cos(i_rad), 0.0, d * np.sin(i_rad)])      
+    r1_0 = -m2/m1 * r2_0                                                          
 
-    # initial velocity vectors of Earth and Moon:
-    v0 = np.array([0.0, V_MOON, 0.0])
-    v_e0 = -M_MOON/(M_EARTH + M_MOON) * v0
-    v_m0 = M_EARTH/(M_EARTH + M_MOON) * v0
+    # initial velocity vectors:
+    v0 = np.array([0.0, v0, 0.0])
+    v1_0 = -m2/(m1 + m2) * v0
+    v2_0 = m1/(m1 + m2) * v0
 
     # initial state vector, [r_e, v_e, r_m, v_m]:
     Z0 = np.concatenate([
-        r_e0,
-        v_e0,
-        r_m0,
-        v_m0
+        r1_0,
+        v1_0,
+        r2_0,
+        v2_0
     ])
 
     def func(t, Z):
-        r_e, v_e, r_m, v_m = np.split(Z, 4)             # unpack state vector (4 equal sub-arrays)
-        r = r_m - r_e                                   # relative position vector of Moon w.r.t. Earth
+        r1, v1, r2, v2 = np.split(Z, 4)             # unpack state vector (4 equal sub-arrays)
+        r = r2 - r1                                   # relative position vector of Moon w.r.t. Earth
         r_mag = np.linalg.norm(r)                       # magnitude (Earth-Moon distance)
-        F = ( G*M_EARTH*M_MOON / r_mag**3 ) * r         # force vector (Newton's law of gravitation)
-        a_e = F / M_EARTH                               # acceleration of Earth
-        a_m = -F / M_MOON                               # acceleration of Moon (equal and opposite)
-        return np.concatenate([v_e, a_e, v_m, a_m])     # return the time derivative of the state vector
+        F = ( G*m1*m2 / r_mag**3 ) * r         # force vector (Newton's law of gravitation)
+        a1 = F / m1                               # acceleration of Earth
+        a2 = -F / m2                               # acceleration of Moon (equal and opposite)
+        return np.concatenate([v1, a1, v2, a2])     # return the time derivative of the state vector
 
     # ----- EVALUATION & SOLVE ----- #
-    orbital_period = 2 * np.pi * np.sqrt(D_EARTH_MOON**3 / (G * (M_EARTH + M_MOON)))  # orbital period of the Earth-Moon system
+    orbital_period = 2 * np.pi * np.sqrt(d**3 / (G * (m1 + m2)))  # orbital period of the Earth-Moon system
     T = T_days * 24 * 3600                                  # one lunar orbit (s)
     t_span = (0, T) if T > 0 else (0, orbital_period) 
     t_eval = np.linspace(t_span[0], t_span[1], steps)       # time points at which to store the solution
     dt = t_eval[1] - t_eval[0]                              # time eval step size
 
-    print(f"\nrunning ODE solver ({method=})...")
+    print(f"\nrunning ODE solver ({ode_method=})...")
     print(f"t_eval=({t_eval[0]:.0f}, {t_eval[-1]}), {steps=:,}, dt≈{dt:.2f}")#
-    sol = solve_ivp(func, t_span, Z0, t_eval=t_eval, method="RK45")
+    sol = solve_ivp(func, t_span, Z0, t_eval=t_eval, method=ode_method)
 
     # ----- EXTRACT RESULTS ----- #
     t, Z = sol.t, sol.y
@@ -84,7 +80,7 @@ def plot_orbits2d(
     moon_markersize: int = 40,
     figure_size: tuple = (8, 8),
     figure_title: str = None,
-    linewidth: float = 0.75,
+    line_width: float = 0.75,
     grid_alpha: float = 0.15,
     x_axis_max_ticks: int = 5,
     y_axis_max_ticks: int = 5,
@@ -147,10 +143,10 @@ def plot_orbits2d(
 
 if __name__ == "__main__":
 
-    t, Z, p = earth_moon_system(
-        T_days=27.321,
-        steps=5000,
-    )
+
+    # ----- SIMULATE EARTH-MOON SYSTEM (DEFAULT PARAMETERS) ----- #
+
+    t, Z, p = two_body_system(steps=5000)
 
     plot_orbits2d(
         t, Z, p,
