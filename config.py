0fbc0534f0d94cf4b00058b75c46d35445de1d77
Anwarat Gurung <agurung_01@outlook.com>
2025-06-08 01:20:27 +0100

    Create a PlotConfig3D dataclass for 3D plotting and animation parameters.

--- config.py
+++ config.py
@@ -22,19 +22,19 @@ class SystemParams:
 @dataclass
 class PlotConfig:
     """Configuration for plotting a two-body system."""
-    # figure properties
+    # figure properties:
     figure_size: Tuple[int, int] = (10, 10)
     figure_title: Optional[str]  = None
     grid_alpha: float = 0.15
     dashed_line_alpha: float = 0.1
     dashed_line_width: float = 0.8
-    # axis configuration
+    # axis configuration:
     x_axis_max_ticks: int = 5
     y_axis_max_ticks: int = 5
     x_axis_limits: Optional[Tuple[float, float]] = None
     y_axis_limits: Optional[Tuple[float, float]] = None
     max_axis_extent: float = 1.05
-    # colours and styling
+    # colours and styling:
     body1_colour: str = "tab:blue"
     body1_trail_colour: str = "tab:blue"
     body2_colour: str = "tab:grey"
@@ -42,20 +42,44 @@ class PlotConfig:
     body1_markersize: int = 1000
     body2_markersize: int = 100
     line_width: float = 0.7
-    # display options
-    show_legend: bool = False
-    show_bc: bool = False
-    bc_colour: str = "tab:red"
-    bc_legend_label: str = "barycentre"
-    bc_alpha: float = 0.8
-    bc_markersize: int = 30
-    # planetary body properties (defaults for Earth-Moon system)
+    trail_length_pct: int = 10  
+    trail_length_factor: float = 3.0
+    # display options:
+    display_legend: bool = False
+    display_baryc: bool = False
+    baryc_colour: str = "tab:red"
+    baryc_legend_label: str = "barycentre"
+    baryc_alpha: float = 0.8
+    baryc_markersize: int = 30
+    # planetary body properties (defaults for Earth-Moon system):
     to_scale: bool = False
     body1_radius: float = R_EARTH
     body1_legend_label: Optional[str] = None
     body2_radius: float = R_MOON
     body2_legend_label: Optional[str] = None
-    # 3D plotting options
+
+
+@dataclass
+class PlotConfig3D():
+    """Extra configuration parameters for 3D plots (on top of the existing 2D plot configurations) for a two-body system."""
+    # figure properties:
+    figure_title: Optional[str] = None
+    body1_markersize: int = 600
+    body2_markersize: int = 200
+    display_legend: bool = False
+    display_baryc: bool = True
+    baryc_markersize: int = 20
+    # if markers are to scale:
+    markers_to_scale: bool = False
+    sphere_alpha: float = 0.8
+    sphere_res: int = 50
+    # axis configuration:
     z_axis_limits: Optional[Tuple[float, float]] = None
     z_axis_max_ticks: int = 5
     draw_dashes3d: bool = True
+    # animation parameters:
+    elev_start: int = 30
+    azim_start: int = -60
+    camera_pan: bool = False
+    elev_end: Optional[int] = None
+    azim_end: Optional[int] = None
